58ad051ee0a7b6f8fff6eed0276033af332e4f14
amfever505 <amfever505@gmail.com>
2021-11-03 18:10:31 +0900

    add js

---
+++ js/index.js
@@ -0,0 +1,10 @@
+import { VctrApi } from 'https://www.vectary.com/viewer-api/v1/api.js';
+
+const viewerApi = new VctrApi('52f7eb92-d707-4c91-bc24-909d653c0ff1');
+
+async function run() {
+  await viewerApi.init();
+  console.log(await viewerApi.getObjects());
+}
+
+run();
